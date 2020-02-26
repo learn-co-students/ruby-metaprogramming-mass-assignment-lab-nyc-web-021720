3f92b0e61bdc9609e7f70089b848f10fc9b45576
JackyLin2 <jacky.jia.lin@gmail.com>
2020-02-26 17:14:36 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-  #your code here
+  @@all = []  
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :glove_size, :wrist_size,:pant_length, :pant_width 
+  
+  def initialize(attribute)
+     attribute.each {|key, value| self.send(("#{key}="), value)}
+  end 
+
 end
